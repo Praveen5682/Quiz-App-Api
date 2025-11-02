1e45278bccb5d1f7f9f0c99a2035d27c0595b382
Praveen Kumar <praveensofficial1@gmail.com>
2025-11-02 17:25:32 +0530

    Added ca.pem file

--- config/db.js
+++ config/db.js
@@ -1,20 +1,27 @@
-const knex = require("knex");
+const mysql = require("mysql2");
+const fs = require("fs");
+const path = require("path");
 const dotenv = require("dotenv");
 dotenv.config();
 
-const db = knex({
-  client: "mysql2",
-  connection: {
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: {
+    ca: fs.readFileSync(path.join(__dirname, "ca.pem")),
+    rejectUnauthorized: true,
   },
 });
 
-// Test the connection
-db.raw("SELECT 1")
-  .then(() => console.log("✅ Database connected successfully"))
-  .catch((err) => console.error("❌ Database connection failed:", err));
+connection.connect((err) => {
+  if (err) {
+    console.error("❌ Database connection failed:", err);
+  } else {
+    console.log("✅ Securely connected to TiDB Cloud Db!");
+  }
+});
 
-module.exports = db;
+module.exports = connection;
